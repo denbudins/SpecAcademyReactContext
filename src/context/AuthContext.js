f75e75175ebdcc9c84966c5e46ced40307580c0d
Denis Martin Budinski <budinski.denis@gmail.com>
2020-04-29 18:44:10 +0200

    Added AuthContext fail

---
+++ src/context/AuthContext.js
@@ -0,0 +1,20 @@
+import React, {useState, createContext} from 'react';
+
+//Define context
+const AuthContext = createContext();
+
+//Define provider
+const AuthProvider = (props) => {
+    const {isLogedIn, setIsLogedIn} = useState(false);
+
+    return(
+        <AuthContext.Provider value={isLogedIn}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+}
+
+//Define consumer
+const AutoConsumer = AuthContext.Consumer;
+
+export {AuthContext, AuthProvider, AutoConsumer};
